0b67665ed96609143b862662927f502aafbfe27a
Emily F <foxxemusicmail@gmail.com>
2020-05-29 23:29:59 +0100

    feat: update manifest and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Emily Fox Music`,
+        short_name: `Emily Fox`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/rose.svg`,
       },
     },
     `gatsby-plugin-resolve-src`,
