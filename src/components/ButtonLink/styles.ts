b3a016206a084c569e69cafd08b0d98cede6192c
Emily F <foxxemusicmail@gmail.com>
2020-05-30 19:58:14 +0100

    feat: change button into a link

--- src/components/ButtonLink/styles.ts
+++ src/components/ButtonLink/styles.ts
@@ -1,23 +1,37 @@
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 import { fontFamily } from 'styles/fonts';
 
-export const ButtonStyle = styled.button`
+export const Button = styled(Link)`
   background: black;
   color: white;
-  border: none;
+  border: black solid 0.25rem;
 
-  padding: 1rem 1.5rem;
+  padding: 0.75rem 1.5rem;
   border-radius: 0.25rem;
 
   font-family: ${fontFamily};
   font-size: 1.5rem;
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
+    background: white;
+    color: black;
+    border: black solid 0.25rem;
+
+    & div {
+      border-color: black;
+    }
+  }
 `;
 
 export const Arrow = styled.div`
   margin-left: 1rem;
 
-  border: solid white;
+  border: solid;
+  border-color: white;
   border-width: 0 0.25rem 0.25rem 0;
   display: inline-block;
   padding: 0.3rem;
