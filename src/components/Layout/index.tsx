84a19472f42c1c876c6f5b3b246eadb60368eb48
Emily F <foxxemusicmail@gmail.com>
2020-08-29 23:32:42 +0100

    feat: nav and image on all pages, flesh out more pages

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,68 @@
+import { useStaticQuery, graphql } from 'gatsby';
 import React, { PropsWithChildren } from 'react';
 import { Normalize } from 'styled-normalize';
 
+import ButtonLink from 'components/ButtonLink';
 import Header from 'components/Header';
 import GlobalFonts from 'fonts/globalFonts';
 
-import { PageContainer } from './styles';
+import {
+  PageContainer,
+  PortraitBackground,
+  MainContentContainer,
+  ButtonContainer,
+} from './styles';
 
-const Layout: React.FC = ({ children }: PropsWithChildren<{}>) => (
-  <>
-    <Normalize />
-    <GlobalFonts />
-    <Header />
-    <PageContainer>{children}</PageContainer>
-  </>
-);
+type LayoutProps = {
+  fullHeightNav?: boolean;
+  links: {
+    text: string;
+    path: string;
+  }[];
+};
+
+const Layout: React.FC<LayoutProps> = ({
+  links,
+  fullHeightNav,
+  children,
+}: PropsWithChildren<{}>) => {
+  const { portrait } = useStaticQuery(
+    graphql`
+      query {
+        portrait: file(relativePath: { eq: "portrait.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const imageData = portrait.childImageSharp.fluid;
+
+  return (
+    <>
+      <Normalize />
+      <GlobalFonts />
+      <Header />
+      <PageContainer>
+        <PortraitBackground
+          fluid={imageData}
+          isFullHeight={fullHeightNav ?? false}>
+          <MainContentContainer>
+            <ButtonContainer>
+              {links.map(({ text, path }) => (
+                <ButtonLink to={path}>{text}</ButtonLink>
+              ))}
+            </ButtonContainer>
+          </MainContentContainer>
+        </PortraitBackground>
+        {children}
+      </PageContainer>
+    </>
+  );
+};
 
 export default Layout;
--- src/components/TextLink/index.tsx
+++ src/components/TextLink/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { Link } from './styles';
 
 type TextLinkProps = {
+  className?: string;
   href: string;
 };
 
-const TextLink: React.FC<TextLinkProps> = ({ href, children }) => (
-  <Link href={href}>{children}</Link>
+const TextLink: React.FC<TextLinkProps> = ({ href, className, children }) => (
+  <Link className={className} href={href}>{children}</Link>
 );
 
 export default TextLink;
