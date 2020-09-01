e4d049e93f13e267f898b2fa1f0dc1a1aa82d646
Emily Feinson <emily.feinson@softwire.com>
2020-09-01 19:20:04 +0100

    style: prettier fix in icons file

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,5 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faYoutube, faFacebook, faBandcamp } from '@fortawesome/free-brands-svg-icons';
+import {
+  faYoutube,
+  faFacebook,
+  faBandcamp,
+} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faEnvelope, faYoutube, faFacebook, faBandcamp);
