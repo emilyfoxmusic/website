6b8e90e80bb5e0132bd2eac34e2a2a25dbeeb855
Emily F <foxxemusicmail@gmail.com>
2020-09-06 23:04:03 +0100

    feat: require onClick handlers for all links

--- src/components/ButtonLink/index.tsx
+++ src/components/ButtonLink/index.tsx
@@ -5,14 +5,20 @@ import { Arrow, Button } from './styles';
 type ButtonLinkProps = {
   to: string;
   className?: string;
+  onClick: React.MouseEventHandler<HTMLAnchorElement>;
 };
 
 const ButtonLink: React.FC<PropsWithChildren<ButtonLinkProps>> = ({
   to,
   className,
+  onClick,
   children,
 }) => (
-  <Button to={to} activeClassName="current-page" className={className}>
+  <Button
+    to={to}
+    activeClassName="current-page"
+    className={className}
+    onClick={onClick}>
     {children}
     <Arrow />
   </Button>
