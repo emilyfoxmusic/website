768677b6d0519d56bf27e1e03ab1431683aa70a4
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:41:42 +0100

    fix: add redux store provider to SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Normalize } from 'styled-normalize';
 import GlobalFonts from 'fonts/globalFonts';
+import StoreProvider from 'components/Live/StoreProvider';
 
 import 'utils/icons';
+
+export const wrapRootElement = ({ element }) => (
+  <StoreProvider>{element}</StoreProvider>
+);
 
 export const wrapPageElement = ({ element }) => (
   <>
