f52cd3660dfab4fc9cde14984591f7c9b35884f4
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:04 +0100

    fix: 404 button width

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,10 +5,15 @@ import ButtonLink from 'components/ButtonLink';
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
 import { PageHeading } from 'components/Typography';
+import { largeBreakpoint } from 'helpers/breakpoints';
 import { count } from 'helpers/goatcounter';
 
 const MusicButton = styled(ButtonLink)`
-  width: 250px !important;
+  max-width: 250px;
+  
+  ${largeBreakpoint`
+    width: 250px !important;
+  `}
   margin: 32px auto;
 `;
 
