334f5ad2f69992d784a525303615434a9c86aa42
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:04 +0100

    fix: wrap pages with fonts rather than including in layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Analytics from './src/components/Analytics';
+import { Normalize } from 'styled-normalize';
+import GlobalFonts from 'fonts/globalFonts';
 
 export const wrapPageElement = ({ element, props }) => (
   <>
+    <Normalize />
+    <GlobalFonts />
     {element}
     <Analytics location={props.location} />
   </>
