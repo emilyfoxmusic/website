c1e1190194b718102ff42e2922b30c0a503d0489
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:01 +0100

    feat: adjust font sizes

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -4,14 +4,14 @@ import { fontFamily } from 'styles/fonts';
 
 export const PageHeading = styled.h1`
   font-family: ${fontFamily};
-  font-size: 1.6rem;
+  font-size: 2rem;
   text-decoration: underline;
   font-weight: normal;
 `;
 
 export const PageHeadingNoUnderline = styled.h1`
   font-family: ${fontFamily};
-  font-size: 1.6rem;
+  font-size: 2rem;
   font-weight: normal;
 `;
 
