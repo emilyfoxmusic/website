6af30379e535484396050625e33a3e840ccc11bc
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:01 +0100

    setup: add styled-normalize plugin

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,12 @@
 import React, { PropsWithChildren } from 'react';
+import { Normalize } from 'styled-normalize';
 
 import Header from 'components/Header';
 import GlobalFonts from 'fonts/globalFonts';
 
 const Layout: React.FC = ({ children }: PropsWithChildren<{}>) => (
   <>
+    <Normalize />
     <GlobalFonts />
     <Header />
     <main>{children}</main>
