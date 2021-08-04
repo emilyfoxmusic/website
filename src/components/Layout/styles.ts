ab758ace982e36962ed6ca640cac4417b37deb9e
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:01 +0100

    feat: style main container

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { fontFamily } from 'styles/fonts';
+
+export const PageContainer = styled.main`
+  max-width: 1200px;
+  margin: auto;
+
+  font-family: ${fontFamily};
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+`;
