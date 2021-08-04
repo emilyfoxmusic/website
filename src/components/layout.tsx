18a7e50e58becda1e8abe58654154e747097ea8e
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:00 +0100

    feat: POC fonts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,12 @@
 import React, { PropsWithChildren } from 'react';
 
 import Header from './header';
+import GlobalFonts from '../fonts/globalFonts';
 
 const Layout: React.FC = ({ children }: PropsWithChildren<{}>) => {
   return (
     <>
+      <GlobalFonts />
       <Header />
       <div>
         <main>{children}</main>
