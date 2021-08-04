b8d2c40b5e13b3ac3c8f3c6f8174448a9cd9f692
Emily F <emily@emilyfoxmusic.co.uk>
2021-08-04 19:37:00 +0100

    feat: do simple SEO.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Emily Fox Music`,
+    description: `The home of Emily Fox Music.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
